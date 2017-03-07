0db83ba2963c52da9dd4c5bd3b9ad0c1989e4551
Art <Julieun@Mac-Tower2.local>
2017-03-07 00:20:46 -0800

    testing saving to repository

---
+++ assets/js/scripts.js
@@ -0,0 +1,145 @@
+
+function scroll_to_class(element_class, removed_height) {
+	var scroll_to = $(element_class).offset().top - removed_height;
+	if($(window).scrollTop() != scroll_to) {
+		$('html, body').stop().animate({scrollTop: scroll_to}, 0);
+	}
+}
+
+function bar_progress(progress_line_object, direction) {
+	var number_of_steps = progress_line_object.data('number-of-steps');
+	var now_value = progress_line_object.data('now-value');
+	var new_value = 0;
+	if(direction == 'right') {
+		new_value = now_value + ( 100 / number_of_steps );
+	}
+	else if(direction == 'left') {
+		new_value = now_value - ( 100 / number_of_steps );
+	}
+	progress_line_object.attr('style', 'width: ' + new_value + '%;').data('now-value', new_value);
+}
+
+jQuery(document).ready(function() {
+	
+    /*
+        Fullscreen background
+    */
+    $.backstretch("assets/img/backgrounds/1.jpg");
+    
+    $('#top-navbar-1').on('shown.bs.collapse', function(){
+    	$.backstretch("resize");
+    });
+    $('#top-navbar-1').on('hidden.bs.collapse', function(){
+    	$.backstretch("resize");
+    });
+    
+    /*
+        Form
+    */
+    $('.f1 fieldset:first').fadeIn('slow');
+    
+    $('.f1 input[type="text"], .f1 input[type="password"], .f1 textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    // next step
+    $('.f1 .btn-next').on('click', function() {
+    	var parent_fieldset = $(this).parents('fieldset');
+    	var next_step = true;
+    	// navigation steps / progress steps
+        var current_active_step = $(this).parents('.f1').find('.f1-step.active');
+    	var checkmark = $(this).parents('.f1').find('.f1-step.active p span');
+    	var progress_line = $(this).parents('.f1').find('.f1-progress-line');
+    	
+    	// fields validation
+    	parent_fieldset.find('input[type="text"], input[type="password"], textarea').each(function() {
+    		if( $(this).val() == "" ) {
+    			$(this).addClass('input-error');
+    			next_step = false;
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	// fields validation
+    	
+    	if( next_step ) {
+    		parent_fieldset.fadeOut(400, function() {
+    			// change icons
+                current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+                checkmark.html($('<img>',{class:'checkmark',src:'./assets/img/checkmark-white.png'}))
+    			// progress bar
+    			bar_progress(progress_line, 'right');
+    			// show next step
+	    		$(this).next().fadeIn();
+	    		// scroll window to beginning of the form
+    			scroll_to_class( $('.f1'), 20 );
+                src="../img/checkmark.png"
+	    	});
+    	}
+    	
+    });
+    
+    // previous step
+    $('.f1 .btn-previous').on('click', function() {
+    	// navigation steps / progress steps
+    	var current_active_step = $(this).parents('.f1').find('.f1-step.active');
+    	var progress_line = $(this).parents('.f1').find('.f1-progress-line');
+    	
+    	$(this).parents('fieldset').fadeOut(400, function() {
+    		// change icons
+    		current_active_step.removeClass('active').prev().removeClass('activated').addClass('active');
+    		// progress bar
+    		bar_progress(progress_line, 'left');
+    		// show previous step
+    		$(this).prev().fadeIn();
+    		// scroll window to beginning of the form
+			scroll_to_class( $('.f1'), 20 );
+    	});
+    });
+    
+    // submit
+    $('.f1').on('submit', function(e) {
+    	
+    	// fields validation
+    	$(this).find('input[type="text"], input[type="password"], textarea').each(function() {
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	// fields validation
+    	
+    });
+    $(function() {
+
+  //We can attach the `fileselect` event to all file inputs on the page
+  $(document).on('change', ':file', function() {
+    var input = $(this),
+        numFiles = input.get(0).files ? input.get(0).files.length : 1,
+        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    input.trigger('fileselect', [numFiles, label]);
+  });
+
+  // We can watch for our custom `fileselect` event like this
+
+  $(':file').on('fileselect', function(event, numFiles, label) {
+
+      var input = $(this).parents('.input-group').find(':text'),
+          log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+      if( input.length ) {
+          input.val(log);
+      } else {
+          if( log ) alert(log);
+      }
+
+  });
+    
+});
+    
+    
+});
